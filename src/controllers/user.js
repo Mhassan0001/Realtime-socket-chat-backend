78265b0d543b3c9115708ee3385d727f8a4ea884
Mhassan0001 <hassanfareed5050@gmail.com>
2026-02-26 02:26:37 +0500

    feat: Add User schema, controller, and routes boilerplate

---
+++ src/controllers/user.js
@@ -0,0 +1,9 @@
+import user from "../models/user";
+
+const createUser = (req, res) => {
+
+
+};
+
+
+export { createUser }
---
+++ src/routes/user.js
@@ -0,0 +1,7 @@
+import express from "express";
+
+const router = express.Router();
+
+router.post("/createUser", user);
+
+export default router;
