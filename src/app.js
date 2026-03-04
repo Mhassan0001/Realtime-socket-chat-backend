8d9d84deffe9e641eb92ea831456af273f9d2a90
Mhassan0001 <hassanfareed5050@gmail.com>
2026-03-05 01:34:41 +0500

    feat: enable CORS for API requests

--- src/app.js
+++ src/app.js
@@ -5,12 +5,13 @@ import connectDB from "./config/db.js";
 import morgan from "morgan";
 import errorHandler from "./middleware/errorHandler.js";
 import authRouter from "../src/routes/auth.js";
+import cors from "cors";
 const app = express();
 const port = process.env.PORT || 9000;
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json());
 app.use(morgan("dev"));
 app.use("/auth", authRouter);
-
 app.get("/", (req, res) => {
   res.json({ msg: "Server Running Successfully....." });
 });
