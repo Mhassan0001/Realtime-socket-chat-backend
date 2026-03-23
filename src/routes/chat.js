450ed428ebc3c1dd47b71dc45d6dcbb5f91c822a
Mhassan0001 <hassanfareed5050@gmail.com>
2026-03-23 22:14:40 +0500

    WIP: partial implementation

--- src/routes/chat.js
+++ src/routes/chat.js
@@ -1,11 +1,12 @@
 import express from "express";
-import { getMessage, sendMessage } from "../controllers/chat.js";
+import { getMessage, sendMessage, createRoom } from "../controllers/chat.js";
 import { auth, roleBase } from "../middleware/auth.js";
 const router = express.Router();
 //!==========================================
 
 router.route("/room/:id").get(auth, getMessage);
 router.route("/send").post(auth, sendMessage);
+router.route("/createRoom").post(auth, createRoom);
 
 //!==========================================
 export default router;
